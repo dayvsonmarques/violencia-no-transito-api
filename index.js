f27b250d006e2f083a10cd9dddc85b189e7b9d28
Dayvson Marques <dayvson.marques@gmail.com>
2020-01-29 19:44:13 -0300

    reading csv

---
+++ index.js
@@ -0,0 +1,15 @@
+const db = require('./config/db.js');
+const acidentesRepository = require('./repository/acidentesRepository');
+
+const csv = require('csv-parser');
+const fs = require('fs');
+
+fs.createReadStream('./acidentesREC2019.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row);
+    //acidentesRepository.create(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed.');
+  });
